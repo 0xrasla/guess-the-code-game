2d1e835bdfd0ad6cd06ac986e0ad2cc15a5e033a
Rasla <programmer.rasla@gmail.com>
2022-02-08 15:31:17 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Code Make Good</h1>
+    </div>
+  );
+}
+
+export default App;
--- public/env.js
+++
@@ -1,3 +0,0 @@
-window.env = {
-  "API_URL": "https://api.github.com/gists/public?page="
-};
